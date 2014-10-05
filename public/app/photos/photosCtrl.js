9be5111944186eb648145f646074600fb82fff01
bugsancho <bugsancho@gmail.com>
2014-10-05 15:41:47 +0300

    Gallery photo should now redirect to details on click

--- public/app/photos/photosCtrl.js
+++ public/app/photos/photosCtrl.js
@@ -1,3 +1,9 @@
-app.controller('PhotosCtrl', function($scope,PhotosResource) {
+app.controller('PhotosCtrl', function($scope,PhotosResource,$location) {
     $scope.photos = PhotosResource.query();
+
+    $scope.redirectToDetails = redirectToDetails;
+
+    function redirectToDetails(id) {
+        $location.path('#/pictures/' + id);
+    }
 });

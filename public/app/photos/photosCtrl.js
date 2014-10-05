73286e26449301e8922f68dc1da7af8f907be5ab
bugsancho <bugsancho@gmail.com>
2014-10-05 17:37:15 +0300

    added client paging

--- public/app/photos/photosCtrl.js
+++ public/app/photos/photosCtrl.js
@@ -1,9 +1,25 @@
-app.controller('PhotosCtrl', function($scope,PhotosResource,$location) {
+app.controller('PhotosCtrl', function ($scope, PhotosResource, $location) {
     $scope.photos = PhotosResource.query();
-
     $scope.redirectToDetails = redirectToDetails;
-
+    $scope.page = 1;
     function redirectToDetails(id) {
         $location.path('photos/' + id);
     }
+
+    function getPhotos(params) {
+        PhotosResource.query(params).$promise.then(function (data) {
+            $scope.photos = data;
+            console.log(data);
+        })
+    }
+
+    $scope.increasePage = function () {
+        $scope.page++;
+        getPhotos({page: $scope.page - 1});
+    };
+    $scope.decreasePage = function () {
+        $scope.page--;
+        getPhotos({page: $scope.page - 1});
+
+    };
 });

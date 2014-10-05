85b91371237a9791c3a5055d92479c0084b2b903
TodorVyagov <rambo007@abv.bg>
2014-10-05 18:30:43 +0300

    Changed navbar color. Added some notifications in photo-details.jade

--- public/app/photos/photosDetailsCtrl.js
+++ public/app/photos/photosDetailsCtrl.js
@@ -1,4 +1,24 @@
-app.controller('PhotosDetailsCtrl', function($scope, $routeParams, PhotosResource) {
-    console.log(3);
+app.controller('PhotosDetailsCtrl', function ($scope, $routeParams, PhotosResource, notifier) {
     $scope.photo = PhotosResource.get({id: $routeParams.id});
+
+    $scope.isBought = function () {
+        // TODO: Check if current user has already bought the current picture.
+        notifier.error('ADD LOGIC IsBought!!!!');
+        return true;
+    }
+
+    $scope.buy = function () {
+        // TODO: Add logic if you have enough money
+        notifier.error('ADD LOGIC buy!!!!');
+        haveMoney = true;
+
+        if (haveMoney) {
+            notifier.success('You bought new picture for: $' + ($scope.photo.price || 0));
+        }
+    };
+
+    $scope.download = function(){
+        // TODO: Implement
+        notifier.success('ADD LOGIC: Downloading!');
+    }
 });

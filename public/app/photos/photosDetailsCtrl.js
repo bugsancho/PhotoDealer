532eda112c642fcc1fa5e182f5b4c9b58927e9c6
TodorVyagov <rambo007@abv.bg>
2014-10-05 15:48:19 +0300

    Added pictures details view.

--- public/app/photos/photosDetailsCtrl.js
+++ public/app/photos/photosDetailsCtrl.js
@@ -1,3 +1,3 @@
-app.controller('PhotosDetailsCtrl', function($scope) {
-
+app.controller('PhotosDetailsCtrl', function($scope, $routeParams, PhotosResource) {
+    $scope.photo = PhotosResource.get({id: $routeParams.id});
 });

f4887561f25642348c902fd57ca3ebdb1642500c
TodorVyagov <rambo007@abv.bg>
2014-10-07 20:39:00 +0300

    Not finished admin panel(listing and editing users).

--- public/app/admin/userListCtrl.js
+++ public/app/admin/userListCtrl.js
@@ -1,3 +1,9 @@
 app.controller('UserListCtrl', function($scope, UsersResource) {
     $scope.users = UsersResource.query();
+
+    $scope.addAmount = function(user){
+        // TODO: ADD MONEY!
+        console.log("ADDING MONEY!");
+
+    };
 });

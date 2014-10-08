164193113d3be2ddce4581c9f726879864569bfc
StefanMihaylov <mihaylov.s@gmail.com>
2014-10-09 00:00:43 +0300

    add filtering and sorting in photo page, small fix in UI

--- public/app/photos/photosCtrl.js
+++ public/app/photos/photosCtrl.js
@@ -9,20 +9,65 @@ app.controller('PhotosCtrl', function ($scope, PhotosResource, $location, identi
     }
 
     function getPhotos(params) {
-        PhotosResource.PhotosResource.query(params).$promise.then(function (data) {
-            $scope.photos = data;
-        })
+        PhotosResource.PhotosResource.query(params).$promise
+            .then(function (data) {
+                $scope.photos = data;
+            })
     }
+
+    function filterBy() {
+        var query = {};
+        query['page'] = $scope.page - 1;
+
+        var filter = $scope.filter;
+        if (filter) {
+            if (filter.hasOwnProperty('title') && filter.title) {
+                query['title'] = filter.title;
+            }
+
+            if (filter.hasOwnProperty('authorName') && filter.authorName) {
+                query['authorName'] = filter.authorName;
+            }
+
+            if (filter.hasOwnProperty('category') && filter.category) {
+                query['category'] = filter.category;
+            }
+
+            if (filter.hasOwnProperty('tags') && filter.tags) {
+                query['tags'] = filter.tags;
+            }
+
+            if (filter.hasOwnProperty('price') && filter.price) {
+                query['price'] = filter.price;
+            }
+        }
+
+        if ($scope.sort) {
+            if ($scope.sort.orderBy) {
+                var sort = $scope.sort.orderBy;
+
+                if ($scope.sort.dir) {
+                    sort = '-' + sort;
+                }
+
+                query['sort'] = sort;
+            }
+        }
+
+        getPhotos(query);
+    };
+
+    $scope.decreasePage = function () {
+        $scope.page--;
+        filterBy()
+    };
 
     $scope.increasePage = function () {
         $scope.page++;
-        getPhotos({page: $scope.page - 1});
-    };
-    $scope.decreasePage = function () {
-        $scope.page--;
-        getPhotos({page: $scope.page - 1});
-
+        filterBy()
     };
 
-
+    $scope.filterBy = function () {
+        filterBy();
+    }
 });

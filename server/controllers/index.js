d15bec15dd012e39cfdc1ae417095d8266e0dc35
TodorVyagov <rambo007@abv.bg>
2014-10-09 13:53:44 +0300

    Changed controller routes.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,6 @@
-var usersController = require('../controllers/usersController');
-var photosController = require('../controllers/PhotosController');
-var filesController = require('../controllers/FilesController');
+var usersController = require('./usersController');
+var photosController = require('./PhotosController');
+var filesController = require('./FilesController');
 module.exports = {
     users: usersController,
     photos : photosController,

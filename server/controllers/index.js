ea108ba86cbd987c5e3843d38ba35a90901324ff
Tihomir Arahangelov <angelov.tisho@gmail.com>
2014-10-09 18:48:55 +0300

    Small bug in server/controllers/index.js fixed.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 var usersController = require(__dirname + '/UsersController'),
 	photosController = require(__dirname + '/PhotosController'),
 	filesController = require(__dirname + '/FilesController'),
-	categoriesController = require(__dirname + '/CategoriesController'),module.exports = {
+	categoriesController = require(__dirname + '/CategoriesController')
+
+    module.exports = {
     users: usersController,
     photos : photosController,
     categories : categoriesController,
